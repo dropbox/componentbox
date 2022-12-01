984d9df919ce04f3637b6bfd81c11519a2136232
mramotar <mramotar@dropbox.com>
2022-12-01 09:11:40 -0500

    Deps

--- componentbox/build.gradle.kts
+++ componentbox/build.gradle.kts
@@ -1,10 +1,6 @@
 @file:Suppress("UnstableApiUsage")
 
-import com.vanniktech.maven.publish.JavadocJar.Dokka
-import com.vanniktech.maven.publish.KotlinMultiplatform
-import com.vanniktech.maven.publish.MavenPublishBaseExtension
 import org.jetbrains.dokka.gradle.DokkaTask
-import org.jetbrains.kotlin.gradle.plugin.mpp.apple.XCFramework
 
 plugins {
     kotlin("multiplatform")
@@ -12,6 +8,9 @@ plugins {
     id("com.android.library")
     id("com.vanniktech.maven.publish.base")
     id("org.jetbrains.dokka")
+    id("co.touchlab.faktory.kmmbridge") version libs.versions.kmm.bridge.get()
+    `maven-publish`
+    kotlin("native.cocoapods")
 }
 
 group = "com.dropbox.componentbox"
@@ -23,13 +22,13 @@ kotlin {
         browser()
         binaries.executable()
     }
+    ios()
 
-    val xcf = XCFramework("componentbox")
-    listOf(iosX64(), iosArm64(), iosSimulatorArm64()).forEach {
-        it.binaries.framework {
-            baseName = "componentbox"
-            xcf.add(this)
-        }
+    cocoapods {
+        summary = "ComponentBox"
+        homepage = "https://github.com/dropbox/componentbox"
+        ios.deploymentTarget = "13"
+        version = "0.2.0-alpha01"
     }
 
     sourceSets {
@@ -53,19 +52,7 @@ kotlin {
             }
         }
 
-        val iosX64Main by getting
-        val iosArm64Main by getting
-        val iosSimulatorArm64Main by getting
-        val iosMain by creating {
-            dependsOn(commonMain)
-
-            dependencies {
-            }
-
-            iosX64Main.dependsOn(this)
-            iosArm64Main.dependsOn(this)
-            iosSimulatorArm64Main.dependsOn(this)
-        }
+        val iosMain by getting
     }
 }
 
@@ -95,8 +82,17 @@ tasks.withType<DokkaTask>().configureEach {
     }
 }
 
-configure<MavenPublishBaseExtension> {
-    configure(
-        KotlinMultiplatform(javadocJar = Dokka("dokkaGfm"))
-    )
+addGithubPackagesRepository()
+kmmbridge {
+    githubReleaseArtifacts()
+    githubReleaseVersions()
+    versionPrefix.set("0.2.0-alpha0")
+    spm()
 }
+
+
+mavenPublishing {
+    publishToMavenCentral(com.vanniktech.maven.publish.SonatypeHost.S01)
+    signAllPublications()
+}
+
--- ui/build.gradle.kts
+++ ui/build.gradle.kts
@@ -9,10 +9,8 @@ plugins {
 group = "com.dropbox.componentbox"
 
 kotlin {
-    iosArm64()
-    iosX64()
-    iosSimulatorArm64()
     android()
+    ios()
 
     sourceSets {
         val commonMain by getting {
@@ -23,13 +21,7 @@ kotlin {
             }
         }
 
-        val iosMain by creating {
-            dependsOn(commonMain)
-        }
-        targets.withType<org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget> {
-            val main by compilations.getting
-            main.defaultSourceSet.dependsOn(iosMain)
-        }
+        val iosMain by getting
     }
 }
 
